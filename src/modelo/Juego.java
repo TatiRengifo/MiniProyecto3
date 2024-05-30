a6566c0aa35e76a01e7138cfca4b01dd906035f2
juanValle5 <juan.j.valencia@correounivalle.edu.co>
2024-05-29 21:50:27 -0500

    Terminacion de la clase Juego documentado

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -1,24 +1,29 @@
 package modelo;
 
-import java.util.Random;
+import java.util.Random; //Importarcion de la libreria random
 
 public class Juego {
-    
+//--------------------------------------------------------------------------------
+    //Variables
+        //Variable para la jugada del humano 
     private String jugada; 
-
+        //Array para las opciones de la maquina
     private static final String[] OPCIONES = {"Piedra","Papel","Tijeras"};
-
+        //Variable random para elegir una opcion aleatoria por la maquina
     private Random random;
-
+//--------------------------------------------------------------------------------
+    //Constructores de la clase
+        //Constructor para la jugada Humano
     public Juego(String jugada) {
         this.jugada = jugada;
     }
-
+        //Constructor para vacio para la jugada de la maquina
     public Juego(){
         random = new Random();
     }
 
-
+//--------------------------------------------------------------------------------
+    //Get y Set de la variable jugada
     public String getJugada() {
         return jugada;
     }
@@ -26,8 +31,11 @@ public class Juego {
     public void setJugada(String jugada) {
         this.jugada = jugada;
     }
-
+//--------------------------------------------------------------------------------
+    //Funcion para encontrar la opcion de la maquina
     public String getOpciones() {
         return OPCIONES[random.nextInt(OPCIONES.length)];
     }
+    
 }
+//--------------------------------------------------------------------------------
