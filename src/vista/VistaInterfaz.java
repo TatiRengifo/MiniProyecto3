70c9f337708e93d751532fda9284b15bae72ac8f
AlejandroSierraUV26 <sierra.alejandro@correounivalle.edu.co>
2024-05-29 22:00:16 -0500

    Vista

---
+++ src/vista/VistaInterfaz.java
@@ -0,0 +1,17 @@
+package vista;
+
+import controlador.ControladorJuego; //Importo el controlador para asi conectarlo con la interfaz
+
+public interface VistaInterfaz {
+
+    //Funcion para mostrar la interfaz GUI
+    public void mostrarInterfaz(ControladorJuego controlador);
+    
+    //Funcion para mostrar el resultado del juego
+    public void mostrarResultadoJuego(String resultado);
+    
+    //Funcion para obtener la jugada del humano
+    public String getJugada();
+
+
+}
