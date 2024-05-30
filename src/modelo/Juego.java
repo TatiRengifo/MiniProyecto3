d565a7557708c4fbc7199ce93a6ae45bfd443a13
juanValle5 <juan.j.valencia@correounivalle.edu.co>
2024-05-29 21:46:02 -0500

    Terminacion de la clase Juego

--- src/modelo/Juego.java
+++ src/modelo/Juego.java
@@ -4,12 +4,27 @@ import java.util.Random;
 
 public class Juego {
     
+    private String jugada; 
+
     private static final String[] OPCIONES = {"Piedra","Papel","Tijeras"};
 
     private Random random;
 
+    public Juego(String jugada) {
+        this.jugada = jugada;
+    }
+
     public Juego(){
         random = new Random();
+    }
+
+
+    public String getJugada() {
+        return jugada;
+    }
+
+    public void setJugada(String jugada) {
+        this.jugada = jugada;
     }
 
     public String getOpciones() {
